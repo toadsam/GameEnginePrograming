925dedfb50ed8f6971c488dc443c9199ca1e2abb
toadsam <toadsam@naver.com>
2025-06-04 17:58:58 +0900

    [ADD] 애니메이션 추가

---
+++ Assets/JJH/Scripts/PlayerAttack.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("✅ 마우스 클릭 - 공격 트리거 발생");
+            animator.SetTrigger("attackTrigger"); // Trigger 발동
+        }
+    }
+}
