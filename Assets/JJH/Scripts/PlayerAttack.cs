02ff8c49e9302ba8bcbe5df1cb258b8817ebfacd
toadsam <toadsam@naver.com>
2025-06-07 20:48:29 +0900

    [ADD]  각종 기능들 추가

--- Assets/JJH/Scripts/PlayerAttack.cs
+++ Assets/JJH/Scripts/PlayerAttack.cs
@@ -14,6 +14,8 @@ public class PlayerAttack : MonoBehaviour
 
     void Start()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         animator = GetComponent<Animator>();
         audioSource = GetComponent<AudioSource>();
 
